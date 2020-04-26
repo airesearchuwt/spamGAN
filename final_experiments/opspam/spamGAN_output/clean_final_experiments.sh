9b5b4468f8471850da27ac2b3f834bf3954269b1
hanfeiyu <hanfeiyu@uw.edu>
2020-04-26 15:42:35 -0700

    Fixing final_runner

--- final_experiments/opspam/spamGAN_output/clean_final_experiments.sh
+++ final_experiments/opspam/spamGAN_output/clean_final_experiments.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-ps aux | grep -v grep | grep final | awk {'print $2'} | xargs kill
+#ps aux | grep -v grep | grep final | awk {'print $2'} | xargs kill
 rm -rf `ll | grep usp | awk {'print $9'}`
 rm result/*
