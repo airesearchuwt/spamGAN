7fcbdce056cf1fa5c983e38df0c911665c19d682
hanfeiyu <hanfeiyu@uw.edu>
2020-04-29 23:19:08 -0700

    Try gpt2 settings on transformer encoder

--- custom_texar/custom_helpers.py
+++ custom_texar/custom_helpers.py
@@ -897,6 +897,9 @@ class GPT2ScheduledEmbeddingTrainingHelper(TrainingHelper):
                 sample_ids_sampling = array_ops.gather_nd(sample_ids, where_sampling)
                 inputs_not_sampling = array_ops.gather_nd(
                     base_next_inputs, where_not_sampling)
+                print("context: {}".format(self.context))
+                print("sample_ids_sampling: {}".format(sample_ids_sampling))
+                print("inputs_not_sampling: {}".format(inputs_not_sampling))
 
                 if self._embedding_args_cnt == 2:
                     # Prepare the position embedding of the next step
