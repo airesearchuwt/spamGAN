9a81c0c564c7799ab9cace92796d94fec03704ee
hanfeiyu <hanfeiyu@uw.edu>
2020-05-01 01:07:34 -0700

    Enable scheduled sampling

---
+++ final_experiments/opspam/clean_experiment.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+output_path="spamGAN_output"
+
+ps aux | grep -v grep | grep final | awk {'print $2'} | xargs kill
+rm ./$output_path/ckpt/*
+rm ./$output_path/result/*
+
+experiment_folders=`ls -l $output_path | grep usp | awk {'print $9'}`
+
+for folder in $experiment_folders
+do
+    rm -rf ./$output_path/$folder
+done
