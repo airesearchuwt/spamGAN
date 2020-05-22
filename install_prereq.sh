d5df8694d5b0ba54ac598dbabe7e596af30f8b4c
hanfeiyu <hanfeiyu@uw.edu>
2020-05-22 13:52:49 -0700

    Adjust scripts

--- install_prereq.sh
+++ install_prereq.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-pip3 install tensorflow-estimator==1.14.0 tensorflow-gpu==1.14.0 tensorflow-probabilit==0.7.0 texar==0.2.4
+pip3 install tensorflow-estimator==1.14.0 tensorflow-gpu==1.14.0 tensorflow-probability==0.7.0 texar==0.2.4
         
