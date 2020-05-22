6cc240b16bcef09fe73c2c06fae01a49b6b8604c
hanfeiyu <hanfeiyu@uw.edu>
2020-05-22 13:59:35 -0700

    Adjust scripts

--- install_prereq.sh
+++ install_prereq.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
+ckpt_dir="ckpt"
+
+if [ ! -d "$ckpt_dir" ]
+then
+    mkdir $ckpt_dir
+else
+    rm $ckpt_dir/*
+fi
+
 pip3 install tensorflow-estimator==1.14.0 tensorflow-gpu==1.14.0 tensorflow-probability==0.7.0 texar==0.2.4
         
