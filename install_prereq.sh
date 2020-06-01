47edc7cd2c9bf66bddb92d3507f4f3b22bdc75ee
hanfeiyu <hanfeiyu@uw.edu>
2020-06-01 05:59:11 -0700

    Update install scripts

--- install_prereq.sh
+++ install_prereq.sh
@@ -1,13 +1,31 @@
 #! /bin/bash
 
-ckpt_dir="ckpt"
+eg_ckpt_dir="./ckpt"
+final_outputs="./final_experiments/opspam/spamGAN_output ./final_experiments/yelp/spamGAN_output"
 
-if [ ! -d "$ckpt_dir" ]
+if [ ! -d "$eg_ckpt_dir" ]
 then
-    mkdir $ckpt_dir
+    mkdir $eg_ckpt_dir
 else
-    rm $ckpt_dir/*
+    rm $eg_ckpt_dir/*
 fi
+
+for output in $final_outputs
+do
+    if [ ! -d "$output/ckpt" ]
+    then
+        mkdir $output/ckpt
+    else
+        rm $output/ckpt/*
+    fi
+
+    if [ ! -d "$output/result" ]
+    then
+        mkdir $output/result
+    # else
+    #     rm $output/ckpt/*
+    fi
+done
 
 pip3 install tensorflow-probability==0.7.0 texar==0.2.4
         
